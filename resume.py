03630bef705c3229487afab9b8200781aa13b947
Rex-Imman Robles <reximman03@gmail.com>
2022-02-08 14:22:38 +0800

    done coding the infos of json file now focusing in codin the font size and header of the resume

--- resume.py
+++ resume.py
@@ -22,6 +22,10 @@ First_name = resumeInfos ["basic info"]["First Name"]
 Middle_name = resumeInfos ["basic info"]["Middle Name"]
 age = resumeInfos ["basic info"]["Age"]
 address = resumeInfos ["basic info"]["Address"]
+gender = resumeInfos ["basic info"]["gender"]
+birthday = resumeInfos ["basic info"]["Birthday"]
+religion = resumeInfos ["basic info"]["religion"]
+citizenship = resumeInfos ["basic info"]["citizenship"] 
 
 # Contact info
 contact = resumeInfos["Contact info"]["Contact Num."]
@@ -30,6 +34,21 @@ email = resumeInfos["Contact info"]["Email"]
 # Objectives
 objective = resumeInfos["Objectives"]["Career Objective"]
 
-# 
+# educational background
+educ_tertiary1 = resumeInfos["Educational Backgroud"]["Tertiary"]["School"]
+educ_tertiary2 = resumeInfos["Educational Background"]["Tertiary"]["School Year"]
+educ_secondary1 = resumeInfos["Educational Background"]["Secondary"]["School"]
+educ_secondary2 = resumeInfos["Educational Background"]["Secondary"]["School Year"]
+educ_primary1 = resumeInfos["Educational Background"]["primary"]["School"]
+educ_primary2 = resumeInfos["Educational Background"]["primary"]["School Year"]
+
+# skills
+skill_1 = resumeInfos["Skill"]["Skill 1"]
+skill_2 = resumeInfos["Skill"]["Skill 2"]
+
+# customizing the header
+
+    
+    
 
 pdf.output("ROBLES_REX-IMMAN.pdf")
