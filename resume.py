5332bbfcc3548eb3070c367b105cad17fb1b58a7
Rex-Imman Robles <reximman03@gmail.com>
2022-02-08 13:36:26 +0800

    finish fixing the errno and starting to code now

--- resume.py
+++ resume.py
@@ -12,22 +12,24 @@ pdf = FPDF('P', 'cm', 'A4')
 pdf.add_page()
 
 # Reading the json file
-with open('Assignment 9\resume.json') as datafile:
+with open('resume.json') as datafile:
     resumeInfos = json.load(datafile)
   
 # getting information from json file  
 # Basic Information
-Last_name = resumeInfos["Last name"]
-First_name = resumeInfos["First name"]
-Middle_name = resumeInfos["Middle name"]
-age = resumeInfos["Age"]
-address = resumeInfos["Address"]
+Last_name = resumeInfos ["basic info"]["Last Name"]
+First_name = resumeInfos ["basic info"]["First Name"]
+Middle_name = resumeInfos ["basic info"]["Middle Name"]
+age = resumeInfos ["basic info"]["Age"]
+address = resumeInfos ["basic info"]["Address"]
 
 # Contact info
-contact = resumeInfos["Contact Num."]
-email = resumeInfos["Email"]
+contact = resumeInfos["Contact info"]["Contact Num."]
+email = resumeInfos["Contact info"]["Email"]
 
 # Objectives
-objective = resumeInfos["Career Objective"]
+objective = resumeInfos["Objectives"]["Career Objective"]
+
+# 
 
 pdf.output("ROBLES_REX-IMMAN.pdf")
